92de3b8985951a6d8d2ddec7aa6b063f4ce67001
Lukas <Lukas.petersson.1999@gmail.com>
2021-01-18 16:24:55 +0100

    camera

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Camera.css';
+import Cam, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
 
 
 class Camera extends React.Component {
@@ -9,7 +11,12 @@ class Camera extends React.Component {
 
 	    }
 
+		this.handleTakePhoto = this.handleTakePhoto.bind(this);
     }
+
+	handleTakePhoto (dataUri) {
+	  console.log('takePhoto', dataUri);
+	}
 
     componentDidMount(){
     }
@@ -17,7 +24,13 @@ class Camera extends React.Component {
     render() {
         return (
             <div className="body">
-				cam
+				<Cam
+				isSilentMode = {true}
+				idealFacingMode ={FACING_MODES.ENVIRONMENT}
+				imageType = {IMAGE_TYPES.JPG}
+				idealResolution = {{width: 224, height: 224}}
+				onTakePhoto = { (dataUri) => { this.handleTakePhoto(dataUri); } }
+			   />
             </div>
         );
     }
